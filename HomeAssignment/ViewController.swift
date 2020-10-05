c03bb89dc30ad4b28d933ff13937e224edca0fcc
Snir Ganon <snir@lightstrap.com>
2020-10-05 22:29:40 +0300

    Initial Commit

---
+++ HomeAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeAssignment
+//
+//  Created by Snir Ganon on 05/10/2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
